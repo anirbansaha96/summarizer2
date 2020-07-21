4dd43e944f9bb829e487ff154bda208dd16ad505
Anirban Saha <anirban.saha.1996@gmail.com>
2020-07-21 12:33:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo “\
 [general]\n\
-email = \”your-email@domain.com\”\n\
+email = \”anirban.saha.1996@gmail.com\”\n\
 “ > ~/.streamlit/credentials.toml
 echo “\
 [server]\n\

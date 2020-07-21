bfc7458af5b3db81f981207feb8806bf6f9704de
Anirban Saha <anirban.saha.1996@gmail.com>
2020-07-21 15:38:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ def main():
     st.title("Question Answering POC")
     Question= st.text_input("Please Input the Question")
     if Question!='':
-    st.write("The Answer to : \n ", Question)
-    st.write("\n\n The Correct Answer")
+        st.write("The Answer to : \n ", Question)
+        st.write("\n\n The Correct Answer")
 
 if __name__ == '__main__':
     main()

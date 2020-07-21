272e732e745a926a150f9f21fde04d6724cf9857
Anirban Saha <anirban.saha.1996@gmail.com>
2020-07-21 15:36:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ import streamlit as st
 
 
 def main():
-    st.title("Just Checking")
+    st.title("Question Answering POC")
+    Question= st.text_input("Please Input the Question")
+    if Question!='':
+    st.write("The Answer to : \n ", Question)
+    st.write("\n\n The Correct Answer")
 
 if __name__ == '__main__':
     main()

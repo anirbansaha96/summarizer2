0fcd3e37303ee22f964d8396270a602e4f2599d4
Anirban Saha <anirban.saha.1996@gmail.com>
2020-07-21 17:17:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import streamlit as st
 def main():
     st.title("Question Answering POC")
     Question= st.text_input("Please Input the Question")
-    if Question!='':
+    if Question=='Who am I?':
         st.write("The Answer to : \n ", Question)
-        st.write("\n\n The Correct Answer")
+        st.write("\n\n Anirban Saha")
 
 if __name__ == '__main__':
     main()

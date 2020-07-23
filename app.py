4a9222f744b2e96ade0091701efe532b0d8d7b1d
Anirban Saha <anirban.saha.1996@gmail.com>
2020-07-23 14:13:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
-
+from transformers import pipeline
+summarizer = pipeline("summarization")
 
 def main():
-    st.title("Question Answering POC")
-    Question= st.text_input("Please Input the Question")
-    if Question=='Who am I?':
-        st.write("The Answer to : \n ", Question)
-        st.write("\n\n Anirban Saha")
-
+    st.title("Summarizer")
+    article_text= st.text_input("Please Input the Article To Summarize")
+    if article_text!='':
+        summary=summarizer(article_text, max_length=130, min_length=30)
+        st.write("The Summary is : \n ", summary)
+        
 if __name__ == '__main__':
     main()

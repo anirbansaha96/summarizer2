af230c52b4f54dc82a3413f2237919b3ef48fd3c
Anirban Saha <anirban.saha.1996@gmail.com>
2020-07-23 14:59:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,37 +26,38 @@ def main():
         nltk.download('stopwords')
         import heapq
         sent_num = int(st.text_input('How many sentences long do you want your summary to be?'))
-        formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )  
-        formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
-        sentence_list = nltk.sent_tokenize(article_text)  
+        if sent_num>0:
+            formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )  
+            formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+            sentence_list = nltk.sent_tokenize(article_text)  
 
-        stopwords = nltk.corpus.stopwords.words('english')
-        word_frequencies = {}  
-        for word in nltk.word_tokenize(formatted_article_text):  
-            if word not in stopwords:
-                if word not in word_frequencies.keys():
-                    word_frequencies[word] = 1
-                else:
-                    word_frequencies[word] += 1
+            stopwords = nltk.corpus.stopwords.words('english')
+            word_frequencies = {}  
+            for word in nltk.word_tokenize(formatted_article_text):  
+                if word not in stopwords:
+                    if word not in word_frequencies.keys():
+                        word_frequencies[word] = 1
+                    else:
+                        word_frequencies[word] += 1
 
-        maximum_frequncy = max(word_frequencies.values())
+            maximum_frequncy = max(word_frequencies.values())
 
-        for word in word_frequencies.keys():  
-            word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
-        sentence_scores = {}  
-        for sent in sentence_list:  
-            for word in nltk.word_tokenize(sent.lower()):
-                if word in word_frequencies.keys():
-                    if len(sent.split(' ')) < 30:
-                        if sent not in sentence_scores.keys():
-                            sentence_scores[sent] = word_frequencies[word]
-                        else:
-                            sentence_scores[sent] += word_frequencies[word]
+            for word in word_frequencies.keys():  
+                word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+            sentence_scores = {}  
+            for sent in sentence_list:  
+                for word in nltk.word_tokenize(sent.lower()):
+                    if word in word_frequencies.keys():
+                        if len(sent.split(' ')) < 30:
+                            if sent not in sentence_scores.keys():
+                                sentence_scores[sent] = word_frequencies[word]
+                            else:
+                                sentence_scores[sent] += word_frequencies[word]
 
-        summary_sentences = heapq.nlargest(sent_num, sentence_scores, key=sentence_scores.get)
-        summary = ' '.join(summary_sentences)  
-        st.markdown("# Summary: ")
-        st.write(summary)   
+            summary_sentences = heapq.nlargest(sent_num, sentence_scores, key=sentence_scores.get)
+            summary = ' '.join(summary_sentences)  
+            st.markdown("# Summary: ")
+            st.write(summary)   
     
 if __name__ == '__main__':
     main()

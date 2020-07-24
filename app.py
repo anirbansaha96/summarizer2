6acb0fbbc921a79ba0b4d74af45e284a0d15acc2
Anirban Saha <anirban.saha.1996@gmail.com>
2020-07-24 11:07:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,7 @@ def main():
     st.title("Wikipedia Summarizer")
     url_topull= st.text_input("Enter the Wikipedia URL to pull - ")
     if url_topull!='':
-        scraped_data = urllib.request.urlopen(url_topull)  
-        article = scraped_data.read()
-
-        parsed_article=bs.BeautifulSoup(article,'lxml')
-
-        paragraphs = parsed_article.find_all('p')
-
-        article_text = ""
-
-        for p in paragraphs:  
-            article_text += p.text
-        article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)  
-        article_text = re.sub(r'\s+', ' ', article_text)
-
+        
         import nltk
         nltk.download('punkt')
         nltk.download('stopwords')

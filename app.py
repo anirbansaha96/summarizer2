22df6babebebec540ca4b0ae462a008f56ca03b0
Anirban Saha <anirban.saha.1996@gmail.com>
2020-07-24 11:22:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import bs4 as bs  
 import urllib.request  
 import re
-import wiki_end
+from wiki_end import get_wiki_data
 
 def main():
     st.title("Wikipedia Summarizer")
     url_topull= st.text_input("Enter the Wikipedia URL to pull - ")
     if url_topull!='':
-        article_text=wiki_end.get_wiki_data(url_topull)
+        article_text=get_wiki_data(url_topull)
         import nltk
         nltk.download('punkt')
         nltk.download('stopwords')

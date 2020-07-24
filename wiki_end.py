45c94bdb1ecabb1dc5368c20ec58e3e047eed911
Anirban Saha <anirban.saha.1996@gmail.com>
2020-07-24 11:09:52 +0530

    Create wiki_end.py

---
+++ wiki_end.py
@@ -0,0 +1,16 @@
+class wiki_end():
+  def get_wiki_data(url_topull):
+    scraped_data = urllib.request.urlopen(url_topull)  
+    article = scraped_data.read()
+
+    parsed_article=bs.BeautifulSoup(article,'lxml')
+
+    paragraphs = parsed_article.find_all('p')
+
+    article_text = ""
+
+    for p in paragraphs:  
+        article_text += p.text
+    article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)  
+    article_text = re.sub(r'\s+', ' ', article_text)
+    return article_text
